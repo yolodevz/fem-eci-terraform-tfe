e945588bbd4b3301846d800e7f833f05b29e30a6
michal-wasilewski <michal@thefabricant.com>
2023-09-27 19:20:02 +0100

    feat: setup vcs automation

--- locals.tf
+++ locals.tf
@@ -8,8 +8,9 @@ locals {
   workspace = {
     "fem-eci-tfe" = {
       description = "example description of the workspace"
-      execution_mode = "local"
+      execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "yolodevz/fem-eci-terraform-tfe"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "organization_usernames" {
   default = ["yolodev_user"]
   type = list(string)
 }
+
+variable "github_app_installation_id" {
+  default = 42286414
+  type    = number
+}
+
+variable "github_organization_name" {
+  default = "yolodevz"
+  type    = string
+}
