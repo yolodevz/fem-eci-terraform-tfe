f0264e92dd4e0d0ea345ef1cda12b2444467e05a
michal-wasilewski <michal@thefabricant.com>
2023-09-27 23:37:07 +0100

    feat: make github sync

--- locals.tf
+++ locals.tf
@@ -15,7 +15,7 @@ locals {
 
     "fem-eci-github" = {
       description = "example automation workspace for GitHub resources"
-      execution_mode = "local"
+      execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
