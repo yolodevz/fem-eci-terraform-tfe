471c185c342db40b3d0c109742805848f7ff6b3e
michal-wasilewski <michal@thefabricant.com>
2023-09-27 22:58:21 +0100

    feat: setup github workspace

--- locals.tf
+++ locals.tf
@@ -10,7 +10,14 @@ locals {
       description = "example description of the workspace"
       execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "yolodevz/fem-eci-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+    }
+
+    "fem-eci-github" = {
+      description = "example automation workspace for GitHub resources"
+      execution_mode = "local"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
   }
 }
